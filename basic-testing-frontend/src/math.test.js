0daff24364db229ea473270c24486fbb17f04171
Your Name <urielparavi75@gmail.com>
2025-04-29 16:25:40 +0300

    16. Basic Test File & Project Setup

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,3 +1,8 @@
-import { it } from 'vitest';
+import { it, expect } from 'vitest';
 
-it('');
+import { add } from './math';
+
+it('should summarize all number values in an array', () => {
+  const result = add([1, 2, 3]);
+  expect(result).toBe(6);
+});
