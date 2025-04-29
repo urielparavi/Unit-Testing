6046dd6853f5ccf27a2b3bc12bfe54cbb03ba750
Your Name <urielparavi75@gmail.com>
2025-04-29 22:39:28 +0300

    20. The AAA Pattern - Arrange, Act, Assert

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,6 +3,16 @@ import { it, expect } from 'vitest';
 import { add } from './math';
 
 it('should summarize all number values in an array', () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  // Arrange - Set up everything needed for the test: inputs, variables, mocks, etc.
+  const numbers = [1, 2];
+
+  // Act - Execute the function or code we're testing.
+  const result = add(numbers);
+
+  // Assert - Check the result to make sure it matches expectations.
+  const expectedResult = numbers.reduce(
+    (prevValue, currentValue) => prevValue + currentValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
 });
