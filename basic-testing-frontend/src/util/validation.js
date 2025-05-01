e3894f563d0bd41bffd65687f82784a044e9a140
Your Name <urielparavi75@gmail.com>
2025-05-01 12:21:38 +0300

    30. More Practice!

--- basic-testing-frontend/src/util/validation.js
+++ basic-testing-frontend/src/util/validation.js
@@ -5,7 +5,8 @@ export function validateStringNotEmpty(value) {
 }
 
 export function validateNumber(number) {
-  if (isNaN(number)) {
+  // So if our number is of the type NaN or that is not from the typeof number, we throwing an error
+  if (isNaN(number) || typeof number !== 'number') {
     throw new Error('Invalid number input.');
   }
 }
