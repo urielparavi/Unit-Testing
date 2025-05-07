6cb4da6236a9350ef3df2a2df6955a9eae33d364
Your Name <urielparavi75@gmail.com>
2025-05-07 21:06:18 +0300

    85. Working with Different Testing Environments

--- util/dom.test.js
+++ util/dom.test.js
@@ -1,6 +1,15 @@
 import { it } from 'vitest';
 import { showError } from './dom';
 
+// ***We added this `--environment happy-dom` in package.json***
+// Using `happy-dom` to simulate a browser-like environment in Node.js.
+// This allows us to test DOM-related code (like document.createElement, innerHTML, etc.)
+// without needing a real browser. Great for testing frontend logic in Vitest.
+//  "scripts": {
+//     "start": "http-server -c-1",
+//     "test": "vitest --run --environment happy-dom --reporter verbose"
+//   },
+
 it('first test', () => {
   showError('test');
 });
